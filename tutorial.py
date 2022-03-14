dd853fa4f2accca8094f5b445486a192ab5661be
Bill <bills534@gmail.com>
2022-03-14 07:28:06 -0400

    added objects with physics

--- tutorial.py
+++ tutorial.py
@@ -15,6 +15,35 @@ def draw(space, window, draw_options):
     pygame.display.update()
 
 
+def create_boundaries(space, width, height):
+    rects = [
+        [(width/2, height - 10), (width, 20)],  #floor
+        [(width/2, 10), (width, 20)],           # ceiling
+        [(10, height/2), (20, height)],         # left
+        [(width - 10, height/2), (20, height)]  # right
+    ]
+
+    for pos, size in rects:
+        body = pymunk.Body(body_type=pymunk.Body.STATIC)
+        body.position = pos
+        shape = pymunk.Poly.create_box(body, size)
+        shape.elasticity = 0.4
+        shape.friction = 0.5
+        space.add(body, shape)
+
+
+def create_ball(space, radius, mass):
+    body = pymunk.Body()
+    body.position = (300,300)
+    shape = pymunk.Circle(body, radius)
+    shape.mass = mass
+    shape.elasticity = 0.9
+    shape.friction = 0.5
+    shape.color = (255, 0, 0, 100)
+    space.add(body, shape)
+    return shape
+
+
 def run(window, width, height):
     run = True
     clock = pygame.time.Clock()
@@ -23,6 +52,9 @@ def run(window, width, height):
 
     space = pymunk.Space()
     space.gravity = (0, 981)
+
+    ball = create_ball(space, 30, 10)
+    create_boundaries(space, width, height)
 
     draw_options = pymunk.pygame_util.DrawOptions(window)
 
